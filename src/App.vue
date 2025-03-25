44d1b6b88ec6af1927d80baaf84b99f613e02a7e
NianwenDan <hello@nwdan.net>
2025-03-24 19:25:37 -0700

    update audit component and main css style

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
-  <BaseLayout>
-    <template v-slot:header>
-      <setting-header></setting-header>
-    </template>
-    <template v-slot:content>
-      <setting-content></setting-content>
-    </template>
-  </BaseLayout>
+  <n-config-provider>
+    <BaseLayout>
+      <template v-slot:header>
+        <setting-header></setting-header>
+      </template>
+      <template v-slot:content>
+        <setting-content></setting-content>
+      </template>
+    </BaseLayout>
+  </n-config-provider>
 </template>
 
 <script>
 import BaseLayout from "@/layout/BaseLayout.vue";
 import SettingHeader from "@/components/SettingHeader.vue";
 import SettingContent from "@/components/SettingContent.vue";
+import { NConfigProvider } from 'naive-ui'
 
 export default {
   name: 'App',
@@ -20,19 +23,11 @@ export default {
     BaseLayout,
     SettingHeader,
     SettingContent,
+    NConfigProvider
   },
 }
 </script>
 
 <style scoped>
-.app-container {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, sans-serif;
-  max-width: 1100px;
-  margin: 0 auto;
-  padding-top: 35px;
-}
 
-.settings-container {
-  overflow: hidden;
-}
 </style>
--- src/components/SettingMainContents/Audit.vue
+++ src/components/SettingMainContents/Audit.vue
@@ -1,13 +1,108 @@
 <script>
+import { NDataTable, NSwitch, NForm, NFormItem, NInput, NInputNumber } from "naive-ui";
 export default {
-  name: "Audit"
+  name: "Audit",
+  components: {
+    NSwitch,
+    NDataTable,
+    NForm,
+    NFormItem,
+    NInput,
+    NInputNumber
+  },
+  data() {
+    return {
+      auditSettings: {
+        isActive: true,
+        auditLimit: 100,
+        auditIdFilter: ''
+      },
+      auditDataTable: {
+        pagination: {
+          pageSize: 5
+        },
+        data: [
+          {
+            "auditId": "AUD-2001",
+            "time": "2025-03-24T08:12:45Z",
+            "blockedUrl": "http://malicious-site.com/phishing"
+          },
+          {
+            "auditId": "AUD-2002",
+            "time": "2025-03-24T09:01:17Z",
+            "blockedUrl": "http://suspiciousdownload.net/file.exe"
+          },
+          {
+            "auditId": "AUD-2003",
+            "time": "2025-03-24T09:45:33Z",
+            "blockedUrl": "http://unsecure-connection.org/login"
+          },
+          {
+            "auditId": "AUD-2004",
+            "time": "2025-03-24T10:22:09Z",
+            "blockedUrl": "http://ads-popup.biz/clickme"
+          },
+          {
+            "auditId": "AUD-2005",
+            "time": "2025-03-24T10:59:40Z",
+            "blockedUrl": "http://fake-update-alert.com"
+          },
+          {
+            "auditId": "AUD-2006",
+            "time": "2025-03-25T10:59:40Z",
+            "blockedUrl": "http://a.fake-update-alert.com"
+          }
+        ],
+        columns: [
+          {
+            title: "Audit Id",
+            key: "auditId",
+            filter(value, row) {
+              return row.auditId.includes(value)
+            }
+          },
+          {title: "Time", key: "time"},
+          {title: "Blocked Url", key: "blockedUrl"},
+        ]
+      }
+    }
+  },
+  methods: {
+    filterAuditId() {
+      const inputValue = this.auditSettings.auditIdFilter.trim()
+      if (inputValue) {
+        this.$refs.table.filter({
+          auditId: [inputValue]
+        })
+      } else {
+        this.$refs.table.filter(null) // clear filter if input is empty
+      }
+    }
+  }
 }
 </script>
 
 <template>
-  <div class="setting-item">
-    <div class="setting-title">
-      Some texts in Audit Component
-    </div>
+  <n-form label-width="auto">
+    <n-form-item label="Enable audit log: ">
+      <n-switch v-model:value="auditSettings.isActive"/>
+    </n-form-item>
+
+    <n-form-item v-if="auditSettings.isActive" label="Limit number of audit: ">
+      <n-input-number v-model:value="auditSettings.auditLimit"/>
+    </n-form-item>
+    <n-form-item v-if="auditSettings.isActive" label="Search by audit Id: ">
+      <n-input v-model:value="auditSettings.auditIdFilter" type="text" placeholder="AUD-2001" @input="filterAuditId" clearable />
+    </n-form-item>
+  </n-form>
+
+  <div v-if="auditSettings.isActive">
+    <n-data-table
+        ref="table"
+        :single-line="false"
+        :columns="auditDataTable.columns"
+        :data="auditDataTable.data"
+        :pagination="auditDataTable.pagination"
+    />
   </div>
 </template>
